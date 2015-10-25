1e96ae5e8e7c7e1bfc2d78b734c97fa377bba28c
Benjamin Winterberg <winterbe@gmail.com>
2015-10-25 19:01:36 +0100

    NSCoding

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -8,19 +8,48 @@
 
 import UIKit
 
-class Meal {
+class Meal: NSObject, NSCoding {
+    
+    struct PropertyKey {
+        static let nameKey = "name"
+        static let photoKey = "photo"
+        static let ratingKey = "key"
+    }
+    
+    static let DocumentsDirectory = NSFileManager()
+        .URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first!
+    
+    static let ArchiveURL = DocumentsDirectory.URLByAppendingPathComponent("meals")
+    
+    
     var name: String
     var photo: UIImage?
     var rating: Int
+    
     
     init?(name: String, photo: UIImage?, rating: Int) {
         self.name = name
         self.photo = photo
         self.rating = rating
         
+        super.init()
+        
         if name.isEmpty || rating < 0 {
             return nil
         }
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        let name = aDecoder.decodeObjectForKey(PropertyKey.nameKey) as! String
+        let photo = aDecoder.decodeObjectForKey(PropertyKey.photoKey) as? UIImage
+        let rating = aDecoder.decodeIntegerForKey(PropertyKey.ratingKey)
+        self.init(name: name, photo: photo, rating: rating)
+    }
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(name, forKey: PropertyKey.nameKey)
+        aCoder.encodeObject(photo, forKey: PropertyKey.photoKey)
+        aCoder.encodeObject(name, forKey: PropertyKey.nameKey)
     }
     
 }
